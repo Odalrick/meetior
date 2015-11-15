884115910e5cd9088524427ac5c1d7077c024001
Ulrik Långström <ulrik.langstrom@gmail.com>
2015-11-15 22:05:59 +0100

    Add some testing framework

--- components/App.jsx
+++ components/App.jsx
@@ -1,6 +1,9 @@
-
 App = React.createClass({
   render: ()=> {
-    return <h1>Meetior - hantera dina möten och få direkt återkoppling.</h1>
+    return (
+      <article>
+        <h1>Meetior - hantera dina möten och få direkt återkoppling.</h1>
+      </article>
+    )
   }
 })
