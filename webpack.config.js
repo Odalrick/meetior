dc980cab97a3008ed4a0940c897f504d1a500008
Magnus <magnus.jonneryd@gmail.com>
2015-12-20 21:52:41 +0100

    Add sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ var HtmlwebpackPlugin = require('html-webpack-plugin');
 var ROOT_PATH = path.resolve(__dirname);
 
 module.exports = {
+  devtool: 'source-map',
   entry: [
     path.resolve(ROOT_PATH, 'app/src/index'),
   ],
