db0dc9088679c0489926dd252898dc856c927569
Ulrik Långström <ulrik.langstrom@gmail.com>
2016-02-14 20:10:44 +0100

    Add forgotten file

---
+++ app/src/components/Meeting.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default (props)=> {
+  const data = {
+    description: 'string',
+    tags:['styrgruppsmöte',],
+    startTime: 'ISO-992',
+    duration: 'seconds',
+    participants: 'int',
+    questionnaire: 'uri',
+    owner: 'uri',
+    feedback: 'feedback[]',
+  }
+  return <div>
+    <div><label>Beskrivning<input/></label></div>
+    <div><label>Kategori<input/></label></div>
+    <div><label>Starttid <input/></label></div>
+    <div><label>Längd <input/></label></div>
+    <div><label>Antal deltagare<input type="number"/></label></div>
+    <div><label>röster [{id:4, q1: 'yes', q2:'no', q6:'sometimes'}]</label></div>
+    <div><label>Frågeformulär<input/></label></div>
+    <div><label>Mötessamordnare (ägare)<input/></label></div>
+  </div>
+}
