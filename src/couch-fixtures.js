eaf39aae3e5cc8ee3bba2dddd3f41f1f1a6b8dd6
Ulrik Långström <ulrik.langstrom@gmail.com>
2016-04-10 21:57:59 +0200

    feat(dev): add fixture courses to database

--- src/couch-fixtures.js
+++ src/couch-fixtures.js
@@ -1,11 +1,17 @@
 import fetch from 'node-fetch'
 import co from 'co'
+import R from 'ramda'
 
+import courseFixtures from './db/fixtures/courses'
 
-co( function *() {
+co(function *() {
   try {
-    const res = yield fetch('http://localhost:5984/test-db', {method: 'PUT'})
-    console.log(res.status)
+    yield fetch('http://localhost:5984/test-db', {method: 'DELETE'})
+    yield fetch('http://localhost:5984/test-db', {method: 'PUT'})
+    yield R.map(course => fetch(`http://localhost:5984/test-db/${course._id}`, {
+      method: 'PUT',
+      body: JSON.stringify(course),
+    }), courseFixtures)
   } catch (e) {
     console.log(e)
   }
