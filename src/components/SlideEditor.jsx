2ded805715f97a40474b2347aaa4456c2d3ba877
Magnus <magnus.jonneryd@gmail.com>
2016-05-04 21:52:58 +0200

    feat(admin): render slide editor on parallel with slide

--- src/components/SlideEditor.jsx
+++ src/components/SlideEditor.jsx
@@ -3,17 +3,23 @@ import React from 'react'
 import WYSIWYG from './WYSIWYG'
 import style from './SlideEditor.css'
 
-export default function SlideEditor(props){
-  const {stopEditingSlide, setSlideText, text} = props
+export default function SlideEditor(props) {
+  const {stopEditingSlide, setSlideText, text, isEditing} = props
   const stopEditing = (event) => {
     event.stopPropagation()
     stopEditingSlide()
   }
   return (
-    <div className={style.modalOverlay}>
-      <div className={style.content}>
-        <WYSIWYG  handleEditorChange={setSlideText} text={text}></WYSIWYG>
-	      <button className={style.closeButton} onClick={stopEditing}>&#x2716;</button>
-	    </div>
-    </div>)
+    <div>
+      { isEditing ?
+        <div className={style.modalOverlay}>
+          <div className={style.content}>
+            <WYSIWYG handleEditorChange={setSlideText} text={text}></WYSIWYG>
+            <button className={style.closeButton} onClick={stopEditing}>&#x2716;</button>
+          </div>
+        </div>
+        : null
+      }
+    </div>
+  )
 }
