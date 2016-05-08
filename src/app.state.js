0418ecbaca453badf66e28270353a39bff24ef51
Ulrik Långström <ulrik.langstrom@gmail.com>
2016-05-08 18:17:52 +0200

    docs(app): add outline of app state

---
+++ src/app.state.js
@@ -0,0 +1,36 @@
+const initialState = {
+  router: {},
+  course: {
+    id: '123abc',
+    current: {},
+    draft: {},
+  },
+  lesson: {
+    id: '321abc',
+    current: {},
+    draft: {},
+  },
+  page: {
+    course: 'abc123',
+    lesson: 'ab321',
+  },
+  // courseViewer: {
+  //  courses: [{ _id: 'asdf', title: 'Bidde det' }],
+  //  filter: {},
+  // },
+  menu: {},
+  documents: {
+    'abc123-as': {
+      current: {},
+      draft: {},
+    },
+    'abc123-as1': {
+      current: {},
+      draft: {},
+    },
+    'abc123-as2': {
+      current: {},
+      draft: {},
+    },
+  },
+}
