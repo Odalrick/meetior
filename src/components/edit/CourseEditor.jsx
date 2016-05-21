b654595f55c407c241546dc335838ba787e21ac9
Ulrik Långström <ulrik.langstrom@gmail.com>
2016-05-21 19:27:26 +0200

    fix(client): adjust course editor css to new layout

--- src/components/edit/CourseEditor.jsx
+++ src/components/edit/CourseEditor.jsx
@@ -4,8 +4,6 @@ import FlowPanel from '../FlowPanel.jsx'
 import Card from './Card.jsx'
 import FlatButton from '../input/FlatButton.jsx'
 
-const styles = require('./CourseEditor.css')
-
 export default function CourseEditor(props) {
   const {
     draft,
@@ -27,11 +25,6 @@ export default function CourseEditor(props) {
           {draft.get('lessons').map((lessonLink) =>(
           <div key={lessonLink.get('_id')}>
             <a href={'/' + lessonLink.get('type') + '/' + lessonLink.get('_id')}>{lessonLink.get('title')}</a>
-            <div className={styles.buttons}>
-              <div>
-                <FlatButton onClick={() => editLesson(lessonLink.get('_id'))}>REDIGERA</FlatButton>
-              </div>
-            </div>
           </div>))}
         </FlowPanel>
       </Card>
