0052bc715624d28832db0134cc79396a8cfb3c20
Magnus <magnus.jonneryd@gmail.com>
2016-06-22 20:15:04 +0200

    feat(docker): add some documentation to the readme.md

--- README.md
+++ README.md
@@ -5,6 +5,25 @@ Install docker:
 https://docs.docker.com/engine/installation/windows/
 https://docs.docker.com/engine/installation/linux/ubuntulinux/
 
+### db commands
+To administrate the db image and the container the following commands
+are at your disposal (npm run db <command>):
+**start** - starts db (the db starts with webpack normally)
+**stop** - stops db
+**rm** - remove current container
+**commit** _tag_ - commit current container, you will need to tag it. This
+creates a new image that can be pushed.
+**push** _tag_ - push to docker hub, use tag to reference an image. See
+commit in order to see how you create a new image.
+
+See docker to learn more about images and containers.
+
+To change which version of db runs with webpack you will need to change the
+tag in package.json under the config key.
+
+### If you want to try docker-compose
+I don't think we'll keep this but I'll keep it for a while.
+
 To init environment (be patient can take several minutes for each command):
 docker-compose -f docker-compose-init.yml --verbose up
 
--- db.js
+++ db.js
@@ -2,100 +2,100 @@ import co from 'co'
 import R from 'ramda'
 import util from 'util'
 import {exec, spawn, spawnSync} from 'child_process'
-   
-const DB_CONTAINER='planck-db'
-const DB_IMAGE='planck/db'                              
-const DB_DEFAULT_TAG=process.env.npm_package_config_db_tag
+
+const DB_CONTAINER = 'planck-db'
+const DB_IMAGE = 'planck/db'
+const DB_DEFAULT_TAG = process.env.npm_package_config_db_tag
 
 const wrapExec = (cmd) => new Promise((resolve, reject) => {
   console.log(cmd)
   exec(cmd,
-	(error, stdout, stderr) => {		
-		if(error){			
-			reject(R.assoc('stderr', stderr, error))
-		}
-		console.log(stdout)
-		resolve(stdout.trim().slice(1,-1))
-	})  
-})  
-  
- const wrapSpawn = (cmd, params) => new Promise((resolve, reject) => {
-  console.log(cmd)  
-  const p = spawn(cmd, params, { stdio: 'inherit' });	
-	p.on('close', (code) => {	  
-	  resolve(code)
-	}); 
-	p.on('error', (code) => {	  
-	  reject(code)
-	}); 
-}) 
-  
-const getContainerId = (tag) => 
-  wrapExec(`docker ps -f name=${DB_CONTAINER} -f ancestor=${DB_IMAGE}:${tag} -a --format \'{{.ID}}\'`)	
-  
+    (error, stdout, stderr) => {
+      if (error) {
+        reject(R.assoc('stderr', stderr, error))
+      }
+      console.log(stdout)
+      resolve(stdout.trim().slice(1, -1))
+    })
+})
+
+const wrapSpawn = (cmd, params) => new Promise((resolve, reject) => {
+  console.log(cmd)
+  const p = spawn(cmd, params, {stdio: 'inherit'});
+  p.on('close', (code) => {
+    resolve(code)
+  });
+  p.on('error', (code) => {
+    reject(code)
+  });
+})
+
+const getContainerId = (tag) =>
+  wrapExec(`docker ps -f name=${DB_CONTAINER} -f ancestor=${DB_IMAGE}:${tag} -a --format \'{{.ID}}\'`)
+
 const login = () => {
-	console.log('Authentication required!')			
-	return wrapSpawn('docker', ['login'])
-} 
- 
+  console.log('Authentication required!')
+  return wrapSpawn('docker', ['login'])
+}
+
 // Commands
-const start = (id, tag) => (id ? wrapExec(`docker start ${DB_CONTAINER}`) :	
+const start = (id, tag) => (id ? wrapExec(`docker start ${DB_CONTAINER}`) :
   wrapExec(`docker run -d -p 5984:5984 --name=${DB_CONTAINER} ${DB_IMAGE}:${tag}`))
-	
+
 const stop = (id) => wrapExec(`docker stop ${id}`)
 
 const rm = (id) => {
-	if(!id){
-		throw `No ${DB_CONTAINER} container to remove`
-	}
-	return wrapExec(`docker rm ${id}`)
+  if (!id) {
+    throw `No ${DB_CONTAINER} container to remove`
+  }
+  return wrapExec(`docker rm ${id}`)
 }
 
-const commit = (id, tag) =>  {
-	if(!tag){
-		throw 'You must provide a tag when commiting a container: \n\t usage: commit <tag>'
-	}
-	wrapExec(`docker commit ${id} ${DB_IMAGE}:${tag}`)
+const commit = (id, tag) => {
+  if (!tag) {
+    throw 'You must provide a tag when committing a container: \n\t usage: commit <tag>'
+  }
+  wrapExec(`docker commit ${id} ${DB_IMAGE}:${tag}`)
 }
 
-const push = function*(id, tag){
-	if(!tag){
-		throw 'You must provide a tag when pushing to docker hub: \n\t usage: push <tag>'
-	}	
-	yield login()	
-	yield wrapExec(`docker push ${DB_IMAGE}:${tag}`)
+const push = function*(id, tag) {
+  if (!tag) {
+    throw 'You must provide a tag when pushing to docker hub: \n\t usage: push <tag>'
+  }
+  yield login()
+  yield wrapExec(`docker push ${DB_IMAGE}:${tag}`)
 }
 
 //main
-co(function*(){		
-	const cmd = process.argv[2]
-	const tag = process.argv[3]		
-	const id = yield getContainerId(tag)	
-	switch(cmd){
-		case 'start': 
-			yield start(id, process.argv[3] || DB_DEFAULT_TAG)
-			break
-		case 'stop': 
-			yield stop(id, process.argv[3] || DB_DEFAULT_TAG) 	
-			break
-		case 'rm': 
-			yield rm(id)
-			break
-		case 'commit': 
-			yield commit(id, process.argv[3])
-			break
-		case 'push':			
-			yield push(id, process.argv[3])
-			break
-		default:
-			console.log(`Unknown command: ${cmd}`)		
-	}
-  }).catch(onerror);
+co(function*() {
+  const cmd = process.argv[2]
+  const tag = process.argv[3]
+  const id = yield getContainerId(tag)
+  switch (cmd) {
+    case 'start':
+      yield start(id, process.argv[3] || DB_DEFAULT_TAG)
+      break
+    case 'stop':
+      yield stop(id, process.argv[3] || DB_DEFAULT_TAG)
+      break
+    case 'rm':
+      yield rm(id)
+      break
+    case 'commit':
+      yield commit(id, process.argv[3])
+      break
+    case 'push':
+      yield push(id, process.argv[3])
+      break
+    default:
+      console.log(`Unknown command: ${cmd}`)
+  }
+}).catch(onerror);
 
 function onerror(err) {
   // log any uncaught errors
   // co will not throw any errors you do not handle!!!
-  // HANDLE ALL YOUR ERRORS!!! 
-  console.error(err && err.stderr || err ); 
+  // HANDLE ALL YOUR ERRORS!!!
+  console.error(err && err.stderr || err);
 }
 
