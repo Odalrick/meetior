67e96a127ce82f4c6a9ddde4ff1f9afbcc0f5f3f
Magnus <magnus.jonneryd@gmail.com>
2016-08-22 21:11:25 +0200

    feat(users): pass user duck tests

--- src/ducks/user.js
+++ src/ducks/user.js
@@ -1,12 +1,24 @@
 import I from 'immutable'
+import jsonWebToken from 'jsonwebtoken'
 
 const LOGIN = 'planck/user/login'
 const LOGOUT = 'planck/user/logout'
+const ERROR = 'planck/user/error'
 
 const initialState = I.fromJS({jwt:'', payload:{}})
 
 const actions = {
-  [LOGIN](state, action) {},
+  [LOGIN](state, action) {
+    const jwt = action.payload.jwt
+    const payload = jsonWebToken.decode(jwt)
+    return state.set('jwt', jwt).set('payload', I.fromJS(payload))
+  },
+  [LOGOUT](state, action) {
+    return initialState
+  },
+  [ERROR](state, action) {
+    return state.set('error', action.payload.message)
+  }
 }
 
 export default function reducer(state = initialState, action) {
@@ -19,6 +31,18 @@ export default function reducer(state = initialState, action) {
 
 export function login(jwt) {
   return {
-    jwt
+    type: LOGIN, payload: { jwt },
+  }
+}
+
+export function logout() {
+  return {
+    type: LOGOUT,
+  }
+}
+
+export function error(message) {
+  return {
+    type: ERROR, error: true, payload: { message }
   }
 }
--- src/ducks/user.spec.js
+++ src/ducks/user.spec.js
@@ -1,10 +1,40 @@
 import reducer from './user'
 import I from 'immutable'
+import {login, logout, error} from './user'
 
 describe('user duck', () => {
+  const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
+
   it('should create initial state', () => {
     const newState = reducer(undefined, {jwt:''})
     expect(newState).equal(I.fromJS({jwt:'',payload:{}}))
   })
+
+  it('should extract payload from jwt', () => {
+    const newState = reducer(undefined, login(jwt))
+    expect(newState).equal(I.fromJS({ jwt, payload: {
+      "sub": "1234567890",
+      "name": "John Doe",
+      "admin": true
+    }}));
+  })
+
+  it('should remove jwt and payload', () => {
+    const newState = reducer(undefined, logout())
+    expect(newState).equal(I.fromJS({ jwt: '', payload: {}}));
+  })
+
+  it('should set error message', () => {
+    const message = "A foreseen error has occured"
+    const newState = reducer(undefined, error(message))
+    expect(newState).equal(I.fromJS({ jwt:'', payload:{}, error: message}))
+  })
+
+  it('should leave jwt alone if an error occurs', () => {
+    const message = "A foreseen error has occured"
+    const newState = reducer(I.fromJS({jwt:'1.2.3', payload:{}}), error(message))
+    expect(newState).equal(I.fromJS({ jwt:'1.2.3', payload:{}, error: message}))
+  })
+
 })
 
