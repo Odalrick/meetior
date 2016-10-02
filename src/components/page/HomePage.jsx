cfe51823c44306812ed1a348543ca05e9b6b8f5d
Ulrik Långström <ulrik.langstrom@gmail.com>
2016-10-02 20:50:25 +0200

    style(HomePage): fix style warnings

--- src/components/page/HomePage.jsx
+++ src/components/page/HomePage.jsx
@@ -1,41 +1,49 @@
+/* global window: false */
 import React from 'react'
 import { connect } from 'react-redux'
 const styles = require('./HomePage.less')
 
-import {loginDispatch} from '../../ducks/commonActions'
+import { loginDispatch } from '../../ducks/commonActions'
 
-function mapStateToProps(state, ownProps) {
+function mapStateToProps(state) {
   return {
-    loggedIn: state.user.get('role') !== 'anonymous',
-    pendingLogin: state.user.get('pending', false)
+    role: state.user.get('role'),
   }
 }
 
-function mapDispatchToProps(dispatch, ownProps) {
+function mapDispatchToProps(dispatch) {
   return {
-    login: loginDispatch(dispatch)
+    login: loginDispatch(dispatch),
   }
 }
 
-function HomePage(props){
-  let nameInput = null
-  let passInput = null
+function HomePage(props) {
+  let nameInput
+  let passInput
   const login = (e) => {
     e.preventDefault()
     const url = `${window.location.protocol}//${window.location.host}/login`
     props.login(url, nameInput.value, passInput.value)
   }
   return (
-  <div className={styles.loginpage}>
-  <h1 className={styles.title}>PLANCK</h1>
-  <h3 className={styles.subtitle}>L E A R N I N G</h3>
-      <form className={styles.loginform}>
-        <input type="text" placeholder="Namn" ref={(ref) => nameInput = ref }></input>
-        <input className={styles.password} type="password" placeholder="Lösenord" ref={(ref) => passInput = ref}></input>
-        <button className={styles.loginBtn} onClick={login}>Logga in</button>
-        <p className={styles.message}>Inte registrerad? <a className={styles.createAccountText} href="#">Skapa ett konto</a></p>
+    <div className={styles.loginpage} >
+      <h1 className={styles.title} >PLANCK</h1>
+      <h3 className={styles.subtitle} >L E A R N I N G</h3>
+      <div>Welcome as a {props.role} user!</div>
+      <form className={styles.loginform} >
+        <input type="text" placeholder="Namn" ref={(ref) => {nameInput = ref}} />
+        <input
+          className={styles.password}
+          type="password"
+          placeholder="Lösenord"
+          ref={(ref) => { passInput = ref}}
+        />
+        <button className={styles.loginBtn} onClick={login} >Logga in</button>
+        <p className={styles.message} >Inte registrerad?
+          <a className={styles.createAccountText} href="#" >Skapa ett konto</a>
+        </p>
       </form>
-  </div>
+    </div>
   )
 }
 
